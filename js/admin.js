1934042be8221542917c049c5da94c965aaa4200
LucasCorbalan <lucas.corbalan.23@gmail.com>
2023-09-14 16:10:09 -0300

    add new validationes

--- js/admin.js
+++ js/admin.js
@@ -1,27 +1,50 @@
 let campoCodigo = document.getElementById("codigo");
 let campoProducto = document.getElementById("producto");
 let campoDescripcion = document.getElementById("descripcion");
-let campoCantidad = document.getElementById("Cantidad");
+let campoCantidad = document.getElementById("cantidad");
 let campoURL = document.getElementById("URL");
 
 /* validaciones */
 const campoRequerido = (input) => {
-  console.log("campoRequerido");
+  console.log("campo Requerido");
   console.log(input.value);
   if (input.value.length > 0) {
     console.log("todo bien");
-    input.className = "form-label is-valid";
+    input.className = "form-control is-valid";
     return true;
   } else {
     console.log("error");
-    input.className = "form-label is-invalid";
+    input.className = "form-control is-invalid";
     return false;
   }
 };
 
+const validarNumeros = (input) => {
+  //vamos a usar o a crear una expresión regular
+  let patron = /^[0-9]{1,3}$/;
+  //el método test me sirve para comparar un string con el patron y duevuelve true o false si hay match o no
+  //regex.test('stirng a validar')
+  if (patron.test(input.value)) {
+    //cumpla con la expresión regular
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
+};
 
-
-
+const validarURL = (input) => {
+  let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+  if (patron.test(input.value)) {
+    //cumpla con la expresión regular
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
+};
 
 /* eventos */
 campoCodigo.addEventListener("blur", () => {
